c937f2ef269a36adad2b6dfcb543b0fd94387807
wachuuu <gab.wachowski15@gmail.com>
2022-04-11 19:03:11 +0200

    basic layout and styles

--- app/src/main/java/com/example/rpncalc/MainActivity.kt
+++ app/src/main/java/com/example/rpncalc/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.rpncalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
