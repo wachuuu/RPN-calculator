8b21302eb425c10cc52f7c5c10086d80a8301765
wachuuu <gab.wachowski15@gmail.com>
2022-04-11 21:34:43 +0200

    additional math operators

--- app/src/main/java/com/example/rpncalc/MainActivity.kt
+++ app/src/main/java/com/example/rpncalc/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.rpncalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun numberClicked(v: View) {
+        val b: Button = v as Button
+        val row: TextView = findViewById(R.id.row0)
+        if (row.text == getString(R.string.zero)) {
+            row.text = b.text.toString()
+        } else {
+            row.text = row.text.toString().plus(b.text.toString())
+        }
+    }
+
+    fun clear(v: View) {
+        val row: TextView = findViewById(R.id.row0)
+        row.text = getString(R.string.zero)
     }
 }
